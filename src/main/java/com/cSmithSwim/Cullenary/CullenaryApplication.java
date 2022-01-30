4949359c48d95737da964d2ee1d3eb28217b8786
Cullen S <cullensmi@gmail.com>
2022-01-29 19:11:36 -0500

    first commit

---
+++ src/main/java/com/cSmithSwim/Cullenary/CullenaryApplication.java
@@ -0,0 +1,13 @@
+package com.cSmithSwim.Cullenary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CullenaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CullenaryApplication.class, args);
+	}
+
+}
