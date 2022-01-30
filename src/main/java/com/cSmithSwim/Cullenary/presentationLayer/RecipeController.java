d528cf435d5567650aa43bb08e60afc6f67fec94
Cullen S <cullensmi@gmail.com>
2022-01-30 10:05:56 -0500

    added presentation layer package and RecipeController from previous project

---
+++ src/main/java/com/cSmithSwim/Cullenary/presentationLayer/RecipeController.java
@@ -0,0 +1,53 @@
+package com.cSmithSwim.Cullenary.presentationLayer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import  culinary.example.demo.businessLayer.Recipe;
+import  culinary.example.demo.businessLayer.RecipeService;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class RecipeController {
+    final RecipeService recipeService;
+
+    @Autowired
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @PostMapping("/recipe/new")
+    public Object postRecipe(@Valid @RequestBody Recipe recipe) {
+        recipe.setDate(LocalDateTime.now());
+        Recipe newRecipe = recipeService.save(recipe);
+        return String.format("{\"id\": %d}", newRecipe.getId());
+    }
+
+    @GetMapping("/recipe/{id}")
+    public Recipe getRecipe(@PathVariable long id) {
+        return recipeService.findRecipeById(id);
+    }
+
+    @DeleteMapping("/recipe/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteRecipe(@PathVariable long id) {
+        recipeService.deleteRecipeById(id);
+    }
+
+    @PutMapping("/recipe/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void updateRecipe(@PathVariable long id, @Valid @RequestBody Recipe recipe) {
+        recipeService.updateRecipe(id, recipe);
+    }
+
+    @GetMapping("/recipe/search/")
+    public List<Recipe> searchRecipeByCategory(@RequestParam Map<String, String> allParams) {
+        return recipeService.searchRecipesBy(allParams);
+    }
+
+}
